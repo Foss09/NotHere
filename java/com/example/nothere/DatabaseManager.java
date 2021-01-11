666008d87c2a7567cd439915ac4cbbfc77b662c8
Foss09 <65361519+Foss09@users.noreply.github.com>
2021-01-11 19:45:50 +0100

    Add files via upload

---
+++ java/com/example/nothere/DatabaseManager.java
@@ -0,0 +1,98 @@
+package com.example.nothere;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DatabaseManager {
+    private static String DATA_BASE_NAME="nothere_db.db";
+    private static int DATA_BASE_VERSION=1;
+    private DBhelper dBhelper;
+    private Context context;
+    private SQLiteDatabase theDataBase;
+
+    public DatabaseManager(Context con){
+        context=con;
+    }
+    private class DBhelper extends SQLiteAssetHelper {
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            super.onUpgrade(db, oldVersion, newVersion);
+        }
+
+        public DBhelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+    }
+    public DatabaseManager open(){
+        dBhelper = new DBhelper(context,DATA_BASE_NAME,null,DATA_BASE_VERSION);
+        theDataBase=dBhelper.getReadableDatabase();
+        return this;
+    }
+    public void close(){
+        dBhelper.close();
+    }
+    public ArrayList<String[]> cityNames(){
+        //this function is to get all the city names in our data base
+        String[] cols= {"Name","Country","Table_name"};
+        Cursor c= theDataBase.query("cities_table",cols,null,null,null,null,null);
+        ArrayList<String[]> resultNames=new ArrayList<String[]>();
+        int iName =c.getColumnIndex("Name");
+        int iCountry=c.getColumnIndex("Country");
+        int iTableName=c.getColumnIndex("Table_name");
+        for(c.moveToFirst();!c.isAfterLast();c.moveToNext()){
+            String[] row=new String[3];
+            row[0]=c.getString(iName);
+            row[1]=c.getString(iCountry);
+            row[2]=c.getString(iTableName);
+            resultNames.add(row);
+        }
+        return resultNames;
+
+    }
+    public ArrayList<String[]> getPrices(String tableName,String category){
+        //in here we pass the query to get our prices based on the category from the city table that we wish
+        String[] cols={"Name","Price"};
+        Cursor c =theDataBase.query(tableName,cols,"Category= \""+category+"\"",null,null,null,null,null);
+        int iName=c.getColumnIndex("Name");
+        int iPrice =c.getColumnIndex("Price");
+        ArrayList<String[]> results=new ArrayList<String[]>();
+        for (c.moveToFirst();!c.isAfterLast();c.moveToNext()){
+            String[] row = new String[2];
+            row[0]=c.getString(iName);
+            row[1]=c.getString(iPrice);
+            results.add(row);
+        }
+        return results;
+    }
+    public ArrayList<String> currencies() {
+        //this function is to get all the currencies names in our data base
+        String[] cols = {"Name"};
+        Cursor c = theDataBase.query("exchange_table", cols, null, null, null, null, null);
+        ArrayList<String> resultcurrencies = new ArrayList<String>();
+        int iName = c.getColumnIndex("Name");
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+
+            resultcurrencies.add(c.getString(iName));
+        }
+        return resultcurrencies;
+    }
+    public String rate(String currency){
+        ///As the name hints, it's to get the appropriate exchange rate for the wanted currency against the USD
+        String[] cols ={"Rate"};
+        Cursor c= theDataBase.query("exchange_table",cols,"Name= \""+currency+"\"",null,null,null,null,null);
+        int iRate =c.getColumnIndex("Rate");
+        String result="";
+        for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+            result+=c.getString(iRate);
+        }
+        return result;
+    }
+}
---
+++ java/com/example/nothere/VieweModel.java
@@ -0,0 +1,108 @@
+package com.example.nothere;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.ViewModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+public class VieweModel extends ViewModel {
+    private DatabaseManager databaseManager;
+    private Context context;
+    public VieweModel(Context con){
+        context=con;
+        databaseManager =new DatabaseManager(context);
+    }
+    public ArrayList<String[]> getCities() throws ExecutionException, InterruptedException {
+        //this function links to our background task, and both of them are aiming to get the names of all the cities in the database
+        CitiesGetter citiesGetter=new CitiesGetter();
+
+        return citiesGetter.execute().get();
+    }
+    public ArrayList<String[]> getPrices(String tableName,String cate) throws ExecutionException, InterruptedException {
+        //this function is linked to our background task, and both of them are aiming to get the prices of stuff after filtering down
+        //filters are : cate->category of the product , tableName -> the city table in our data base ::what city did we select
+        //both filters are selected in the activities
+        String[] args= new String[2];
+        args[0]=tableName;
+        args[1]=cate;
+        PriceGetter priceGetter= new PriceGetter();
+        return priceGetter.execute(args).get() ;
+    }
+
+    public ArrayList<String> getCurrencies() throws ExecutionException, InterruptedException {
+        return new CurrenciesGetter().execute().get();
+
+    }
+    public String getRate(String currency) throws ExecutionException, InterruptedException {
+        return new RateGetter().execute(currency).get();
+    }
+
+    public class CitiesGetter extends AsyncTask<Void,Void,ArrayList<String[]>>{
+
+        @Override
+        protected ArrayList<String[]> doInBackground(Void... voids) {
+            databaseManager.open();
+
+            return databaseManager.cityNames();
+        }
+    }
+    public class PriceGetter extends AsyncTask<String[],Void,ArrayList<String[]>>{
+
+        @Override
+        protected ArrayList<String[]> doInBackground(String[]... voids) {
+            String [] args=voids[0];
+            databaseManager.open();
+
+            return databaseManager.getPrices(args[0],args[1]);
+        }
+    }
+    public class CurrenciesGetter extends AsyncTask<Void,Void,ArrayList<String>>{
+
+        @Override
+        protected ArrayList<String> doInBackground(Void... voids) {
+            databaseManager.open();
+            return databaseManager.currencies();
+        }
+    }
+    public class RateGetter extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL url= new URL("https://v6.exchangerate-api.com/v6/7298c00edad1769469b7957c/latest/USD");
+                InputStream inputStream = ((HttpURLConnection) url.openConnection()).getInputStream();
+                Scanner scanner= new Scanner(inputStream);
+                scanner.useDelimiter("\\A");
+                JSONObject jsonString =new JSONObject(scanner.next());
+                JSONObject objy=jsonString.getJSONObject("conversion_rates");
+                return Double.toString(objy.getDouble(strings[0]));
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+    }
+
+
+}
